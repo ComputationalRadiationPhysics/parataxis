2c11e66340453e18137af415479fe89edb0e21be
Alexander Grund <a.grund@hzdr.de>
2015-09-07 17:05:06 +0200

    Add GetTargetCellIdx

--- include/detector/PhotonDetector.hpp
+++ include/detector/PhotonDetector.hpp
@@ -76,5 +76,53 @@ namespace detector {
         }
     };
 
+    /**
+     * Calculates the cell index on the detector that a photon reaches when it continues
+     * with the current speed
+     */
+    struct GetTargetCellIdx
+    {
+        /**
+         *
+         * @param xPosition Position of the detector in x direction
+         * @param size      Size of the detector
+         * @param globalIdx Global index of the current super cell
+         */
+        GetTargetCellIdx(float_X xPosition, Space2D size, Space globalIdx):
+            xPosition_(xPosition), size_(size), globalIdx_(globalIdx)
+        {}
+
+        template<typename T_Particle>
+        HDINLINE bool
+        operator()(const T_Particle& particle, Space2D& targetIdx, float_X& dt)
+        {
+            auto mom = particle[momentum_];
+            /* Not flying towards detector? -> exit */
+            if(mom.x() <= 0)
+                return false;
+            /* Calculate global position */
+            const float_X momAbs = PMaccMath::abs(mom);
+            const floatD_X vel   = mom * ( SPEED_OF_LIGHT / momAbs );
+            floatD_X pos;
+            for(uint32_t i=0; i<simDim; ++i)
+                pos[i] = (float_X(globalIdx_[i]) + particle[position_][i]) * cellSize[i];
+            /* Required time to reach detector */
+            dt = (xPosition_ - pos.x()) / vel.x();
+            /* Position at detector plane */
+            pos.z() += dt * vel.z();
+            pos.y() += dt * vel.y();
+            targetIdx.x() = pos.z() / CELL_DEPTH;
+            targetIdx.y() = pos.y() / CELL_HEIGHT;
+            /* Check bounds */
+            return targetIdx.x() >= 0 && targetIdx.x() < size_.x() &&
+                   targetIdx.y() >= 0 && targetIdx.y() < size_.y();
+        }
+
+    private:
+        PMACC_ALIGN(xPosition_, const float_X);
+        PMACC_ALIGN(size_, const Space2D);
+        PMACC_ALIGN(globalIdx_, const Space);
+    };
+
 }  // namespace detector
 }  // namespace xrt
