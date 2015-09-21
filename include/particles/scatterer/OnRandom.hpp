6b9309b180a2db7c8f823122423269a279f92a20
Alexander Grund <a.grund@hzdr.de>
2015-09-21 16:40:08 +0200

    Add random scattering

---
+++ include/particles/scatterer/OnRandom.hpp
@@ -0,0 +1,48 @@
+#pragma once
+
+#include "xrtTypes.hpp"
+#include "Random.hpp"
+
+namespace xrt {
+namespace particles {
+namespace scatterer {
+
+    /**
+     * Condition functor for scattering that depends on a probability that can be dependent
+     * on the density in the particles cell.
+     * The Config needs a static function calcProbability(float_X density) the returns the
+     * probability for scattering in the range [0, 1]
+     */
+    template<
+        class T_Config,
+        class T_Species = bmpl::_1
+        >
+    struct OnRandom
+    {
+        using Config = T_Config;
+
+        HINLINE explicit
+        OnRandom(uint32_t currentStep): rand(currentStep, seeds::scatterCondition)
+        {}
+
+        HDINLINE void
+        init(Space totalCellIdx)
+        {
+            rand.init(totalCellIdx);
+        }
+
+        template<class T_DensityBox, typename T_Position, typename T_Momentum>
+        HDINLINE bool
+        operator()(const T_DensityBox& density, const T_Position& pos, const T_Momentum& mom)
+        {
+            float_X probability = Config::calcProbability(density(Space::create(0)));
+            return (rand() < probability);
+        }
+
+    private:
+        PMACC_ALIGN(rand, Random<T_Species>);
+    };
+
+}  // namespace scatterer
+}  // namespace particles
+}  // namespace xrt
---
+++ include/particles/scatterer/RandomDirection.hpp
@@ -0,0 +1,48 @@
+#pragma once
+
+#include "xrtTypes.hpp"
+#include "Random.hpp"
+#include <algorithms/math.hpp>
+
+namespace xrt {
+namespace particles {
+namespace scatterer {
+
+    /**
+     * Scatterer that changes the momentum based on 2 random angles (spherical coordinates)
+     */
+    template<class T_Species = bmpl::_1>
+    struct RandomDirection
+    {
+
+        HINLINE explicit
+        RandomDirection(uint32_t currentStep): rand(currentStep, seeds::scatterDirection)
+        {}
+
+        HDINLINE void
+        init(Space totalCellIdx)
+        {
+            rand.init(totalCellIdx);
+        }
+
+        template<class T_DensityBox, typename T_Position, typename T_Momentum>
+        HDINLINE void
+        operator()(const T_DensityBox& density, const T_Position& pos, T_Momentum& mom)
+        {
+            float_X polarAngle   = rand() * float_X(PI);
+            float_X azimuthAngle = rand() * float_X(2 * PI);
+            float_X sinPolar, cosPolar, sinAzimuth, cosAzimuth;
+            PMaccMath::sincos(polarAngle, sinPolar, cosPolar);
+            PMaccMath::sincos(azimuthAngle, sinAzimuth, cosAzimuth);
+            mom.x() = sinPolar * cosAzimuth;
+            mom.y() = sinPolar * sinAzimuth;
+            mom.z() = cosPolar;
+        }
+
+    private:
+        PMACC_ALIGN(rand, Random<T_Species>);
+    };
+
+}  // namespace scatterer
+}  // namespace particles
+}  // namespace xrt
