da269c8908805f1667c2cdb6341154da6931d361
Alexander Grund <a.grund@hzdr.de>
2015-10-06 15:29:47 +0200

    Add energy flag as alternative to wavelength

--- include/particles/functors/GetAngFrequency.hpp
+++ include/particles/functors/GetAngFrequency.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "xrtTypes.hpp"
+#include "particles/functors/GetWavelength.hpp"
 
 namespace xrt {
 namespace particles {
@@ -13,17 +14,12 @@ namespace functors {
     struct GetAngularFrequency
     {
         using Species = T_Species;
-        using FrameType = typename Species::FrameType;
-
-        using HasWavelength = HasFlag_t<FrameType, wavelength<> > ;
-        static_assert(HasWavelength::value, "Species has no wavelength set");
-        using Wavelength = GetResolvedFlag_t<FrameType, wavelength<> >;
 
         HDINLINE float_X
         operator()() const
         {
             // k = 2 * PI / lambda
-            const float_X waveNumber = 2 * float_X(PI) / (Wavelength::getValue() / UNIT_LENGTH);
+            const float_X waveNumber = 2 * float_X(PI) / GetWavelength<Species>()();
             // Assume vacuum in the medium -> w = k * v = k * c
             const float_X omega = waveNumber * SPEED_OF_LIGHT;
             return omega;
---
+++ include/particles/functors/GetWavelength.hpp
@@ -0,0 +1,50 @@
+#pragma once
+
+#include "xrtTypes.hpp"
+
+namespace xrt {
+namespace particles {
+namespace functors {
+
+    namespace detail {
+
+        template<typename T_FrameType, bool hasWavelength = HasFlag_t<T_FrameType, wavelength<> >::value>
+        struct GetWavelength
+        {
+            using FrameType = T_FrameType;
+            using Wavelength = GetResolvedFlag_t<FrameType, wavelength<> >;
+
+            HDINLINE float_X
+            operator()() const
+            {
+                return Wavelength::getValue() / UNIT_LENGTH;
+            }
+
+        };
+
+        template<typename T_FrameType>
+        struct GetWavelength<T_FrameType, false>
+        {
+            using FrameType = T_FrameType;
+            static_assert(HasFlag_t<T_FrameType, energy<> >::value, "Species has no wavelength or energy set");
+            using Energy = GetResolvedFlag_t<FrameType, energy<> >;
+
+            HDINLINE float_X
+            operator()() const
+            {
+                return PLANCK_CONSTANT * SPEED_OF_LIGHT / (Energy::getValue() / UNIT_ENERGY);
+            }
+
+        };
+
+    }  // namespace detail
+
+    /**
+     * Returns the wavelength of a species (unit-less) from wavelength or energy property
+     */
+    template<typename T_Species>
+    using GetWavelength = detail::GetWavelength<typename T_Species::FrameType>;
+
+}  // namespace functors
+}  // namespace particles
+}  // namespace xrt
