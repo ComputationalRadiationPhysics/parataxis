f1f7111865df1e30ba3c3423b867775e47a01292
Alexander Grund <A.Grund@hzdr.de>
2016-03-07 13:37:34 +0100

    Fix param naming

--- configureExample.sh
+++ configureExample.sh
@@ -99,7 +99,7 @@ fi
 
 cd $destinationDir
 
-cmake_command="cmake $cmake_flags -DXRT_EXTENSION_PATH=$exampleDir $this_dir"
+cmake_command="cmake $cmakeFlags -DXRT_EXTENSION_PATH=$exampleDir $this_dir"
 echo -e "\033[32mcmake command:\033[0m $cmake_command"
 $cmake_command
 result=$?
