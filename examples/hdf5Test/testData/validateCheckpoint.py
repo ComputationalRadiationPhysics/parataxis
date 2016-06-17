958ead496ecbf251620a7314f7d204df690496ea
Flamefire <git@grundis.de>
2016-06-17 13:43:43 +0200

    Validate checkpoint too

---
+++ examples/hdf5Test/testData/validateCheckpoint.py
@@ -0,0 +1,34 @@
+import os
+from numpy import *
+import numpy.testing as npt
+import unittest
+import h5py
+
+class TestRestart(unittest.TestCase):
+    def testCheckpoint(self):
+        checkpointPath = os.environ["TEST_SIMOUTPUT_PATH"] + "/checkpoints"
+        hdf5Path = checkpointPath + "/hdf5_checkpoint_100.h5"
+        
+        f = h5py.File(hdf5Path)
+        data = list(f["data"].values())[0]
+        fields = data.get("fields") or data["meshes"]
+        # Number of fields
+        self.assertGreater(len(fields), 0)
+        for field in fields.values():
+            self.assertGreater(len(field), 0)
+            
+        particles = data["particles"]
+        # Number of species
+        self.assertGreater(len(particles), 0)
+        for species in particles.values():
+            momentum = species.get("momentum")
+            self.assertIsNotNone(momentum)
+            # 3D momentum
+            self.assertEqual(len(momentum), 3)
+            # All momentums must be ~1
+            momentums = vstack((momentum["x"], momentum["y"], momentum["z"]))
+            momNorms = linalg.norm(momentums, axis=0)
+            npt.assert_array_almost_equal(momNorms, 1.)
+ 
+if __name__ == '__main__':
+    unittest.main()
