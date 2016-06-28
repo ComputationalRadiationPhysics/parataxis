a4c62694b506ca8bdc5bf3b5a4ae34af5cc16d30
Flamefire <git@grundis.de>
2016-06-28 15:44:37 +0200

    Close HDF5 files on test

--- examples/hdf5Test/testData/validateCheckpoint.py
+++ examples/hdf5Test/testData/validateCheckpoint.py
@@ -9,26 +9,26 @@ class TestRestart(unittest.TestCase):
         checkpointPath = os.environ["TEST_SIMOUTPUT_PATH"] + "/checkpoints"
         hdf5Path = checkpointPath + "/hdf5_checkpoint_100.h5"
         
-        f = h5py.File(hdf5Path)
-        data = list(f["data"].values())[0]
-        fields = data.get("fields") or data["meshes"]
-        # Number of fields
-        self.assertGreater(len(fields), 0)
-        for field in fields.values():
-            self.assertGreater(len(field), 0)
-            
-        particles = data["particles"]
-        # Number of species
-        self.assertGreater(len(particles), 0)
-        for species in particles.values():
-            momentum = species.get("momentum")
-            self.assertIsNotNone(momentum)
-            # 3D momentum
-            self.assertEqual(len(momentum), 3)
-            # All momentums must be ~1
-            momentums = np.vstack((momentum["x"], momentum["y"], momentum["z"]))
-            momNorms = np.linalg.norm(momentums, axis=0)
-            npt.assert_array_almost_equal(momNorms, 1.)
+        with h5py.File(hdf5Path) as f:
+            data = list(f["data"].values())[0]
+            fields = data.get("fields") or data["meshes"]
+            # Number of fields
+            self.assertGreater(len(fields), 0)
+            for field in fields.values():
+                self.assertGreater(len(field), 0)
+                
+            particles = data["particles"]
+            # Number of species
+            self.assertGreater(len(particles), 0)
+            for species in particles.values():
+                momentum = species.get("momentum")
+                self.assertIsNotNone(momentum)
+                # 3D momentum
+                self.assertEqual(len(momentum), 3)
+                # All momentums must be ~1
+                momentums = np.vstack((momentum["x"], momentum["y"], momentum["z"]))
+                momNorms = np.linalg.norm(momentums, axis=0)
+                npt.assert_array_almost_equal(momNorms, 1.)
  
 if __name__ == '__main__':
     unittest.main()
--- examples/hdf5Test/testData/validateRestart.py
+++ examples/hdf5Test/testData/validateRestart.py
@@ -20,34 +20,33 @@ class TestRestart(unittest.TestCase):
             for ds1, ds2 in zip(valIs.values(), valExpected.values()):
                 self.checkRecords(ds1, ds2, sortValues)
 
-    def testCheckpoint(self):
+    def testRestart(self):
         self.longMessage = True
         checkpointPath = os.environ["TEST_SIMOUTPUT_PATH"] + "/checkpoints"
         hdf5Path = checkpointPath + "/hdf5_checkpoint_100.h5"
         hdf5PathOrig = hdf5Path.replace("out_Restart", "out_Checkpoint")
         
-        f1 = h5py.File(hdf5Path)
-        f2 = h5py.File(hdf5PathOrig)
-        data1 = list(f1["data"].values())[0]
-        data2 = list(f2["data"].values())[0]
-        fields1 = data1.get("fields") or data1["meshes"]
-        fields2 = data2.get("fields") or data2["meshes"]
-        # Number of fields
-        self.assertEqual(len(fields1), len(fields2))
-        self.checkRecords(fields1, fields2, False)
-            
-        particles1 = data1["particles"]
-        particles2 = data2["particles"]
-        # Number of species
-        self.assertEqual(len(particles1), len(particles2))
-        for species1, species2 in zip(particles1.values(), particles2.values()):
-            # Number of species attributes
-            self.assertEqual(len(species1), len(species2), "Species: " + species1.name)
-            for prop1, prop2 in zip(species1.values(), species2.values()):
-                self.assertEqual(len(prop1.attrs), len(prop2.attrs), "Species attributes: " + species1.name)
-                for attr1, attr2 in zip(prop1.attrs.items(), prop2.attrs.items()):
-                    npt.assert_equal(attr1[1], attr2[1], err_msg = "Attribute: " + prop1.name + "/" + attr1[0])
-                self.checkRecords(prop1, prop2, True)
+        with h5py.File(hdf5Path) as f1, h5py.File(hdf5PathOrig) as f2:
+            data1 = list(f1["data"].values())[0]
+            data2 = list(f2["data"].values())[0]
+            fields1 = data1.get("fields") or data1["meshes"]
+            fields2 = data2.get("fields") or data2["meshes"]
+            # Number of fields
+            self.assertEqual(len(fields1), len(fields2))
+            self.checkRecords(fields1, fields2, False)
+                
+            particles1 = data1["particles"]
+            particles2 = data2["particles"]
+            # Number of species
+            self.assertEqual(len(particles1), len(particles2))
+            for species1, species2 in zip(particles1.values(), particles2.values()):
+                # Number of species attributes
+                self.assertEqual(len(species1), len(species2), "Species: " + species1.name)
+                for prop1, prop2 in zip(species1.values(), species2.values()):
+                    self.assertEqual(len(prop1.attrs), len(prop2.attrs), "Species attributes: " + species1.name)
+                    for attr1, attr2 in zip(prop1.attrs.items(), prop2.attrs.items()):
+                        npt.assert_equal(attr1[1], attr2[1], err_msg = "Attribute: " + prop1.name + "/" + attr1[0])
+                    self.checkRecords(prop1, prop2, True)
  
     def testDetector(self):
         self.longMessage = True
@@ -55,15 +54,14 @@ class TestRestart(unittest.TestCase):
         hdf5Path = checkpointPath + "/PhotonDetector_checkpoint_100.h5"
         hdf5PathOrig = hdf5Path.replace("out_Restart", "out_Checkpoint")
         
-        f1 = h5py.File(hdf5Path)
-        f2 = h5py.File(hdf5PathOrig)
-        data1 = list(f1["data"].values())[0]
-        data2 = list(f2["data"].values())[0]
-        fields1 = data1.get("fields") or data1["meshes"]
-        fields2 = data2.get("fields") or data2["meshes"]
-        # Number of fields
-        self.assertEqual(len(fields1), len(fields2))
-        self.checkRecords(fields1, fields2, False)
+        with h5py.File(hdf5Path) as f1, h5py.File(hdf5PathOrig) as f2:
+            data1 = list(f1["data"].values())[0]
+            data2 = list(f2["data"].values())[0]
+            fields1 = data1.get("fields") or data1["meshes"]
+            fields2 = data2.get("fields") or data2["meshes"]
+            # Number of fields
+            self.assertEqual(len(fields1), len(fields2))
+            self.checkRecords(fields1, fields2, False)
  
 if __name__ == '__main__':
     unittest.main()
