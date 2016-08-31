4da38c186f11cc6502e696c4a8e72daf8e47cd0f
Flamefire <git@grundis.de>
2016-08-31 12:11:05 +0200

    Switch to XorMin by default

--- include/plugins/hdf5/RngStateBoxReader.hpp
+++ include/plugins/hdf5/RngStateBoxReader.hpp
@@ -3,38 +3,60 @@
 #include "xrtTypes.hpp"
 #include "plugins/hdf5/DataBoxReader.hpp"
 #include <random/methods/Xor.hpp>
+#include <random/methods/XorMin.hpp>
 
 namespace xrt {
 namespace plugins {
 namespace hdf5 {
 
-template<>
-struct DataBoxReader<PMacc::random::methods::Xor::StateType>
-{
-    template<class T_SplashReader, class T_DataBox>
-    void operator()(T_SplashReader& reader, const T_DataBox& dataBox,
-            const PMacc::Selection<simDim>& globalDomain,
-            const PMacc::DataSpace<simDim>& localSize,
-            const PMacc::DataSpace<simDim>& localOffset)
+    template<>
+    struct DataBoxReader<PMacc::random::methods::XorMin::StateType>
     {
-        using ValueType = typename T_DataBox::ValueType;
-        static_assert(std::is_same<PMacc::random::methods::Xor::StateType, ValueType>::value, "Wrong type in dataBox");
-        const PMacc::Selection<simDim> localDomain(localSize, localOffset);
-
-        readDataBox(reader["d"], makeHostTransformBox(dataBox, [](ValueType& state) -> unsigned& { return state.d; }), globalDomain, localDomain);
-        std::array<char, 3> vNames = {'v', '0', '\0'};
-        for(unsigned i=0; i<5; i++)
+        template<class T_SplashReader, class T_DataBox>
+        void operator()(T_SplashReader& reader, const T_DataBox& dataBox,
+                const PMacc::Selection<simDim>& globalDomain,
+                const PMacc::DataSpace<simDim>& localSize,
+                const PMacc::DataSpace<simDim>& localOffset)
         {
-            vNames[1] = char('0' + i);
-            readDataBox(reader[&vNames.front()], makeHostTransformBox(dataBox, [i](ValueType& state) -> unsigned&  { return state.v[i]; }), globalDomain, localDomain);
-        }
+            using ValueType = typename T_DataBox::ValueType;
+            static_assert(std::is_same<
+                    PMacc::random::methods::Xor::StateType, ValueType>::value || // Due to same naming we can also write the common part of Xor
+                    std::is_same<PMacc::random::methods::XorMin::StateType, ValueType>::value
+                    , "Wrong type in dataBox");
+            const PMacc::Selection<simDim> localDomain(localSize, localOffset);
 
-        readDataBox(reader["boxmuller_flag"], makeHostTransformBox(dataBox, [](ValueType& state) -> int&  { return state.boxmuller_flag; }), globalDomain, localDomain);
-        readDataBox(reader["boxmuller_flag_double"], makeHostTransformBox(dataBox, [](ValueType& state) -> int&  { return state.boxmuller_flag_double; }), globalDomain, localDomain);
-        readDataBox(reader["boxmuller_extra"], makeHostTransformBox(dataBox, [](ValueType& state) -> float&  { return state.boxmuller_extra; }), globalDomain, localDomain);
-        readDataBox(reader["boxmuller_extra_double"], makeHostTransformBox(dataBox, [](ValueType& state) -> double&  { return state.boxmuller_extra_double; }), globalDomain, localDomain);
-    }
-};
+            readDataBox(reader["d"], makeHostTransformBox(dataBox, [](ValueType& state) -> unsigned& { return state.d; }), globalDomain, localDomain);
+            std::array<char, 3> vNames = {'v', '0', '\0'};
+            for(unsigned i=0; i<5; i++)
+            {
+                vNames[1] = char('0' + i);
+                readDataBox(reader[&vNames.front()], makeHostTransformBox(dataBox, [i](ValueType& state) -> unsigned&  { return state.v[i]; }), globalDomain, localDomain);
+            }
+        }
+    };
+
+    template<>
+    struct DataBoxReader<PMacc::random::methods::Xor::StateType>
+    {
+        template<class T_SplashReader, class T_DataBox>
+        void operator()(T_SplashReader& reader, const T_DataBox& dataBox,
+                const PMacc::Selection<simDim>& globalDomain,
+                const PMacc::DataSpace<simDim>& localSize,
+                const PMacc::DataSpace<simDim>& localOffset)
+        {
+            using ValueType = typename T_DataBox::ValueType;
+            static_assert(std::is_same<PMacc::random::methods::Xor::StateType, ValueType>::value, "Wrong type in dataBox");
+            const PMacc::Selection<simDim> localDomain(localSize, localOffset);
+
+            // Trick: Read common part of Xor and XorMin
+            DataBoxReader<PMacc::random::methods::XorMin::StateType>()(reader, dataBox, globalDomain, localSize, localOffset);
+
+            readDataBox(reader["boxmuller_flag"], makeHostTransformBox(dataBox, [](ValueType& state) -> int&  { return state.boxmuller_flag; }), globalDomain, localDomain);
+            readDataBox(reader["boxmuller_flag_double"], makeHostTransformBox(dataBox, [](ValueType& state) -> int&  { return state.boxmuller_flag_double; }), globalDomain, localDomain);
+            readDataBox(reader["boxmuller_extra"], makeHostTransformBox(dataBox, [](ValueType& state) -> float&  { return state.boxmuller_extra; }), globalDomain, localDomain);
+            readDataBox(reader["boxmuller_extra_double"], makeHostTransformBox(dataBox, [](ValueType& state) -> double&  { return state.boxmuller_extra_double; }), globalDomain, localDomain);
+        }
+    };
 
 }  // namespace hdf5
 }  // namespace plugins
--- include/plugins/hdf5/RngStateBoxWriter.hpp
+++ include/plugins/hdf5/RngStateBoxWriter.hpp
@@ -8,33 +8,54 @@ namespace xrt {
 namespace plugins {
 namespace hdf5 {
 
-template<>
-struct DataBoxWriter<PMacc::random::methods::Xor::StateType>
-{
-    template<class T_SplashWriter, class T_DataBox>
-    void operator()(T_SplashWriter& writer, const T_DataBox& dataBox,
-            const PMacc::Selection<simDim>& globalDomain,
-            const PMacc::DataSpace<simDim>& localSize,
-            const PMacc::DataSpace<simDim>& localOffset)
+    template<>
+    struct DataBoxWriter<PMacc::random::methods::XorMin::StateType>
     {
-        using ValueType = typename T_DataBox::ValueType;
-        static_assert(std::is_same<PMacc::random::methods::Xor::StateType, ValueType>::value, "Wrong type in dataBox");
-        const PMacc::Selection<simDim> localDomain(localSize, localOffset);
-
-        writeDataBox(writer["d"], makeHostTransformBox(dataBox, [](const ValueType& state) { return state.d; }), globalDomain, localDomain);
-        std::array<char, 3> vNames = {'v', '0', '\0'};
-        for(unsigned i=0; i<5; i++)
+        template<class T_SplashWriter, class T_DataBox>
+        void operator()(T_SplashWriter& writer, const T_DataBox& dataBox,
+                const PMacc::Selection<simDim>& globalDomain,
+                const PMacc::DataSpace<simDim>& localSize,
+                const PMacc::DataSpace<simDim>& localOffset)
         {
-            vNames[1] = char('0' + i);
-            writeDataBox(writer[&vNames.front()], makeHostTransformBox(dataBox, [i](const ValueType& state)  { return state.v[i]; }), globalDomain, localDomain);
-        }
+            using ValueType = typename T_DataBox::ValueType;
+            static_assert(std::is_same<
+                    PMacc::random::methods::Xor::StateType, ValueType>::value || // Due to same naming we can also write the common part of Xor
+                    std::is_same<PMacc::random::methods::XorMin::StateType, ValueType>::value
+                    , "Wrong type in dataBox");
+            const PMacc::Selection<simDim> localDomain(localSize, localOffset);
 
-        writeDataBox(writer["boxmuller_flag"], makeHostTransformBox(dataBox, [](const ValueType& state)  { return state.boxmuller_flag; }), globalDomain, localDomain);
-        writeDataBox(writer["boxmuller_flag_double"], makeHostTransformBox(dataBox, [](const ValueType& state)  { return state.boxmuller_flag_double; }), globalDomain, localDomain);
-        writeDataBox(writer["boxmuller_extra"], makeHostTransformBox(dataBox, [](const ValueType& state)  { return state.boxmuller_extra; }), globalDomain, localDomain);
-        writeDataBox(writer["boxmuller_extra_double"], makeHostTransformBox(dataBox, [](const ValueType& state)  { return state.boxmuller_extra_double; }), globalDomain, localDomain);
-    }
-};
+            writeDataBox(writer["d"], makeHostTransformBox(dataBox, [](const ValueType& state) { return state.d; }), globalDomain, localDomain);
+            std::array<char, 3> vNames = {'v', '0', '\0'};
+            for(unsigned i=0; i<5; i++)
+            {
+                vNames[1] = char('0' + i);
+                writeDataBox(writer[&vNames.front()], makeHostTransformBox(dataBox, [i](const ValueType& state)  { return state.v[i]; }), globalDomain, localDomain);
+            }
+        }
+    };
+
+    template<>
+    struct DataBoxWriter<PMacc::random::methods::Xor::StateType>
+    {
+        template<class T_SplashWriter, class T_DataBox>
+        void operator()(T_SplashWriter& writer, const T_DataBox& dataBox,
+                const PMacc::Selection<simDim>& globalDomain,
+                const PMacc::DataSpace<simDim>& localSize,
+                const PMacc::DataSpace<simDim>& localOffset)
+        {
+            using ValueType = typename T_DataBox::ValueType;
+            static_assert(std::is_same<PMacc::random::methods::Xor::StateType, ValueType>::value, "Wrong type in dataBox");
+            const PMacc::Selection<simDim> localDomain(localSize, localOffset);
+
+            // Trick: Write common part of Xor and XorMin
+            DataBoxWriter<PMacc::random::methods::XorMin::StateType>()(writer, dataBox, globalDomain, localSize, localOffset);
+
+            writeDataBox(writer["boxmuller_flag"], makeHostTransformBox(dataBox, [](const ValueType& state)  { return state.boxmuller_flag; }), globalDomain, localDomain);
+            writeDataBox(writer["boxmuller_flag_double"], makeHostTransformBox(dataBox, [](const ValueType& state)  { return state.boxmuller_flag_double; }), globalDomain, localDomain);
+            writeDataBox(writer["boxmuller_extra"], makeHostTransformBox(dataBox, [](const ValueType& state)  { return state.boxmuller_extra; }), globalDomain, localDomain);
+            writeDataBox(writer["boxmuller_extra_double"], makeHostTransformBox(dataBox, [](const ValueType& state)  { return state.boxmuller_extra_double; }), globalDomain, localDomain);
+        }
+    };
 
 }  // namespace hdf5
 }  // namespace plugins
