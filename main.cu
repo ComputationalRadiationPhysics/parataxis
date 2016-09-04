76ebacdb67c7f2ba4cedd6750dfabc6824bd91b3
Flamefire <git@grundis.de>
2016-09-04 11:25:53 +0200

    Catch and print errors

--- main.cu
+++ main.cu
@@ -7,7 +7,7 @@ int main( int argc, char **argv )
     MPI_CHECK(MPI_Init(&argc, &argv));
 
     int errorCode;
-    {
+    try{
         /* Use nested region to make sure all simulation classes are
          * freed before we call MPI_Finalize
          */
@@ -32,6 +32,11 @@ int main( int argc, char **argv )
                 std::cerr << "Unhandled parser code: " << int(parserCode) << std::endl;
                 errorCode = 99;
         };
+    }catch(std::exception& e)
+    {
+        std::cerr << "Unhandled exception occurred: " << e.what() << std::endl;
+        std::cout << "Unhandled exception occurred: " << e.what() << std::endl;
+        errorCode = 2;
     }
 
     MPI_CHECK(MPI_Finalize());
