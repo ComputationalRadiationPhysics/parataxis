be5b287499dd9f2b42e78fc35dd8f672a219a135
Flamefire <git@grundis.de>
2016-09-15 23:54:32 +0200

    Use 4GPU profiling

--- examples/Diplomarbeit/startProfiling.sh
+++ examples/Diplomarbeit/startProfiling.sh
@@ -2,7 +2,7 @@
 
 rm -rf ~/xrtTestsProfiling
 export TBG_TPLFILE=submit/hypnos-hzdr/k20_nvprof_profile.tpl
-export TBG_CFGFILE=submit/1gpuProfiling.cfg
+export TBG_CFGFILE=submit/4gpuProfiling.cfg
 export TBG_SUBMIT=qsub
 
 buildSystem/runTests.py -e examples/Diplomarbeit/ -o ~/xrtTestsProfiling -t "DoubleSlitRandPos" -t "DoubleSlitRandPosSingle" -j -DCUDA_ARCH=sm_35 -DXRT_NVPROF_NUM_TS=5 -DXRT_NVPROF_START_TS=2998 -r --compile-only && \
