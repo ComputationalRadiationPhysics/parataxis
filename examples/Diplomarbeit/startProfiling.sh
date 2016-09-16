57e5ff96a243d0cc8516e463ccb1f07bd54ccc83
Flamefire <git@grundis.de>
2016-09-16 16:45:59 +0200

    Fix spaces

--- examples/Diplomarbeit/startProfiling.sh
+++ examples/Diplomarbeit/startProfiling.sh
@@ -4,7 +4,7 @@ rm -rf ~/xrtTestsProfiling
 export TBG_TPLFILE=submit/hypnos-hzdr/k20_nvprof_profile.tpl
 export TBG_CFGFILE=submit/8gpuProfiling.cfg
 export TBG_SUBMIT=qsub
-outputFolder = "$HOME/xrtTestsProfiling"
+outputFolder="$HOME/xrtTestsProfiling"
 
 buildSystem/runTests.py -e examples/Diplomarbeit/ -o "$outputFolder" -t "DoubleSlitRandPos" -t "DoubleSlitRandPosSingle" -j -DCUDA_ARCH=sm_35 -DXRT_NVPROF_NUM_TS=5 -DXRT_NVPROF_START_TS=2998 -r --compile-only && \
 cd "$outputFolder"/installed/DiplomarbeitSim_cmake1 && \
